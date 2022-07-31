b1311923b5f860473cb312f02306bda9505ab5e3
airsay <eruotorese@yahoo.com>
2022-07-31 11:00:44 +0100

    solved an 8kyu kata

--- 8kyu/calculateAverage.js
+++ 8kyu/calculateAverage.js
@@ -3,21 +3,21 @@
 // Note: Empty arrays should return 0.
 
 // SOLUTION
-function findAverage(array) {
-    // array would always be numbers
-  //   find the average number of the array
-  //   when found it should be returned
-  //   empty array should return 0
-  //   add all the numbers in the array and divide it by the length of the array
-  //   use the .reduce() array method
-  //   use the conditional statement so the empty array can return 0
+// function findAverage(array) {
+//     // array would always be numbers
+//   //   find the average number of the array
+//   //   when found it should be returned
+//   //   empty array should return 0
+//   //   add all the numbers in the array and divide it by the length of the array
+//   //   use the .reduce() array method
+//   //   use the conditional statement so the empty array can return 0
     
-    if(array.length > 0){
-      let average = array.reduce((n, acc) => n + acc,0) / array.length 
-      return average
-    }
-    else{
-      return 0
-    }
-  }
+//     if(array.length > 0){
+//       let average = array.reduce((n, acc) => n + acc,0) / array.length 
+//       return average
+//     }
+//     else{
+//       return 0
+//     }
+//   }
 
