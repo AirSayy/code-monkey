075960efd76cc417682675c9d354127aece708f9
Ese Eguakhide <eruotorese@yahoo.com>
2022-08-10 14:28:53 +0100

    solved an 8kyu code

---
+++ 8kyu/You-only-need-one.js
@@ -0,0 +1,16 @@
+// You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+
+// Array can contain numbers or strings. X can be either.
+
+// Return true if the array contains the value, false if not.
+
+//SOLUTION
+
+// function check(a, x) {
+//     return a.includes(x) ? true : false
+//   }
+  
+
+//ANOTHER SOLUTION USING THE .some() array method
+
+//const check = (arr, x) => arr.some(item => item == x);
