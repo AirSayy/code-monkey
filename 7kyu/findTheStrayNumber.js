67bb272ed437c884fcb08da688132cca3966bd77
airsay <eruotorese@yahoo.com>
2022-10-05 13:48:54 +0100

    solved a 7kyu kata

--- 7kyu/findTheStrayNumber.js
+++ 7kyu/findTheStrayNumber.js
@@ -1,5 +1,5 @@
-You are given an odd-length array of integers, in which all of them are the same, except for one single number.
+// You are given an odd-length array of integers, in which all of them are the same, except for one single number.
 
-Complete the method which accepts such an array, and returns that single different number.
+// Complete the method which accepts such an array, and returns that single different number.
 
-The input array will always be valid! (odd-length >= 3)
+// The input array will always be valid! (odd-length >= 3)
