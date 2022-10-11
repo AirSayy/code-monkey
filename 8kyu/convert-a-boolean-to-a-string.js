9aac9db46e871eda7b6b83debb23fea9714c4c65
airsay <eruotorese@yahoo.com>
2022-10-11 20:52:04 +0100

    solved an 8kyu kata

---
+++ 8kyu/convert-a-boolean-to-a-string.js
@@ -0,0 +1,11 @@
+// DESCRIPTION:
+// Implement a function which convert the given boolean value into its string representation.
+
+// Note: Only valid inputs will be given.
+
+// SOLUTION
+
+// function booleanToString(b){
+  
+//     return b == true ? "true" : "false"
+//  }
