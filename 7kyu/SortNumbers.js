775b52a46fbd8df2f35925d4af8b4b126b61b1df
Eseoghene Eguakhide <eruotorese@yahoo.com>
2022-11-06 15:14:27 +0100

    solved a kata

--- 7kyu/SortNumbers.js
+++ 7kyu/SortNumbers.js
@@ -1,4 +1,4 @@
-Finish the solution so that it sorts the passed in array of numbers. If the function passes in an empty array or null/nil value then it should return an empty array.
+// Finish the solution so that it sorts the passed in array of numbers. If the function passes in an empty array or null/nil value then it should return an empty array.
 
 For example:
 
