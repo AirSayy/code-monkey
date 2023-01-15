48aab7302bcffdf17a9bc59eb80892a6a36ba257
Eseoghene Eguakhide <eruotorese@yahoo.com>
2023-01-15 12:00:03 +0100

    6kyu kata

--- 6kyu/Find-the-odd-int.js
+++ 6kyu/Find-the-odd-int.js
@@ -1,4 +1,4 @@
-DESCRIPTION:
+// DESCRIPTION:
 Given an array of integers, find the one that appears an odd number of times.
 
 There will always be only one integer that appears an odd number of times.
