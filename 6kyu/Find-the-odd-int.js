89aeff75828b072c3f4e291ef1b76f95b60403b6
Eseoghene Eguakhide <eruotorese@yahoo.com>
2023-01-19 12:05:21 +0100

    6kyu kata

--- 6kyu/Find-the-odd-int.js
+++ 6kyu/Find-the-odd-int.js
@@ -4,7 +4,7 @@
 // There will always be only one integer that appears an odd number of times.
 
 // Examples
-[7] should return 7, because it occurs 1 time (which is odd).
+// [7] should return 7, because it occurs 1 time (which is odd).
 [0] should return 0, because it occurs 1 time (which is odd).
 [1,1,2] should return 2, because it occurs 1 time (which is odd).
 [0,1,0,1,0] should return 0, because it occurs 3 times (which is odd).
