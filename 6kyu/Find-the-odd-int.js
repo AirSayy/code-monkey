dd9889014f6ae2a61265cff2f8cd95d3eabde6f3
Eseoghene Eguakhide <eruotorese@yahoo.com>
2023-01-26 12:12:12 +0100

    6kyu kata

--- 6kyu/Find-the-odd-int.js
+++ 6kyu/Find-the-odd-int.js
@@ -10,4 +10,4 @@
 // [0,1,0,1,0] should return 0, because it occurs 3 times (which is odd).
 // [1,2,2,3,3,3,4,3,3,3,2,2,1] should return 4, because it appears 1 time (which is odd).
 
-// solution
+// solutions
