f672ae57fb08c3c38068122cca4fc3ba46fb1cbe
Eseoghene Eguakhide <eruotorese@yahoo.com>
2023-01-30 11:21:15 +0100

    7kyu kata

--- 7kyu/Round-up-to-the-next-multiple-of-5.js
+++ 7kyu/Round-up-to-the-next-multiple-of-5.js
@@ -1,18 +1,18 @@
-DESCRIPTION:
-Given an integer as input, can you round it to the next (meaning, "greater than or equal") multiple of 5?
+// DESCRIPTION:
+// Given an integer as input, can you round it to the next (meaning, "greater than or equal") multiple of 5?
 
-Examples:
+// Examples:
 
-input:    output:
-0    ->   0
-2    ->   5
-3    ->   5
-12   ->   15
-21   ->   25
-30   ->   30
--2   ->   0
--5   ->   -5
-etc.
-Input may be any positive or negative integer (including 0).
+// input:    output:
+// 0    ->   0
+// 2    ->   5
+// 3    ->   5
+// 12   ->   15
+// 21   ->   25
+// 30   ->   30
+// -2   ->   0
+// -5   ->   -5
+// etc.
+// Input may be any positive or negative integer (including 0).
 
-You can assume that all inputs are valid integers.
+// You can assume that all inputs are valid integers.
