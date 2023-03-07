727dcc47f89f11f3d12ed45f0de6df85551074c1
Eseoghene Eguakhide <eruotorese@yahoo.com>
2023-03-07 11:11:42 +0100

    7kyu

--- 7kyu/Ch4113ng3.js
+++ 7kyu/Ch4113ng3.js
@@ -1,6 +1,6 @@
 // Make your strings more nerdy: Replace all 'a'/'A' with 4, 'e'/'E' with 3 and 'l' with 1 e.g. "Fundamentals" --> "Fund4m3nt41s"
 
-// solution
+// solution.
 
 function nerdify(txt){
     const nerdy = txt.split('').map(a => a.replace(/[aA]/g,'4').replace(/[eE]/g,'3').replace(/l/g,'1')).join('')
